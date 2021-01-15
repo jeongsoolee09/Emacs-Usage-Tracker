bab2c1e210b9cd73f4867ed1e9761d5f24e79046
JeongSoo Lee <volcano0909@gmail.com>
2021-01-15 22:58:05 +0900

    using defcustom, and some function prototypes

--- emacs-usage-tracker.el
+++ emacs-usage-tracker.el
@@ -3,7 +3,9 @@
 (require 'cl-lib)
 
 
-(defconst *track-file-dir* "~/.emacs.d/.emacs-hours")
+(defcustom emacs-track-file-dir "~/.emacs.d/.emacs-hours"
+  "The file for recording Emacs usage time."
+  :type 'file)
 
 
 (defun create-track-file-if-necessary ()
@@ -13,7 +15,7 @@
 
 (defun save-uptime-at-exit ()
   "1. create a temp buffer,
-   2. write the uptime into that buffer,
+   2. write the uptime (in seconds) into that buffer,
    3. and save the buffer, appending to the existing `.emacs-hours` file."
   (let ((date (format-time-string "%Y-%m-%d:"))
         (uptime (concat (emacs-uptime "%s") "\n")))
@@ -29,9 +31,61 @@
     (append-to-file (point-min) (point-max) *track-file-dir*)))
 
 
-(defun main ()
+(defun emacs-usage-tracker-setup ()
+  (interactive)
   (create-track-file-if-necessary)
   (add-hook save-time-at-startup emacs-startup-hook)
   (add-hook save-uptime-at-exit kill-emacs-hook))
+
+
+(defun seconds-to-hours-and-mins (seconds)
+  (format-seconds "%d days, %h hours, %m minutes, and %s seconds"))
+
+
+;; Statistics functions
+
+
+(defun get-usage-of-day (date)
+  "Given a date, get the usage of that day.")
+
+
+(defun get-usage-of-today ()
+  "Get the usage of today.")
+
+
+(defun get-usage-of-week (date)
+  "Get the usage of that week upto the given date.")
+
+
+(defun get-usage-of-this-week ()
+  "Get the usage of this week upto today.")
+
+
+(defun get-usage-of-month (date)
+  "Get the usage of that month upto the given date.")
+
+
+(defun get-usage-of-this-month ()
+  "Get the usage of this month upto today.")
+
+
+(defun get-usage-of-year (date)
+  "Get the usage of that year upto the given date.")
+
+
+(defun get-highest-day-of-month (date)
+  "Get the date with the most uptime in that month,
+   upto the given date.")
+
+
+(defun get-highest-day-of-year (date)
+  "Get the date with the most uptime in that year,
+   upto the given date.")
+
+
+(defun get-highest-month-of-year (date)
+  "Get the month with the most uptime in that year,
+   upto the given date.")
+
 
 ;;; emacs-usage-tracker.el ends here
